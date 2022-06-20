c7ba72e7b875ea9e958b5298ac4e307d2a1bdbf8
Lukas Gillmann <lukasgillmann1@outlook.de>
2022-06-21 04:43:49 +0900

    mui + tailwind with basic protocol

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './index.scss';
 
 import reportWebVitals from './reportWebVitals';
-import './index.scss';
 import { AsterControllerProvider } from './context';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Sample from './pages/Sample';
+import SampleOther from './pages/SampleOther';
+import Page404 from './pages/Page404';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AsterControllerProvider>
-      <App />
-    </AsterControllerProvider>
+    <BrowserRouter>
+      <AsterControllerProvider>
+        <Routes>
+          <Route path='*' element={<Page404 />} />
+          <Route path="/" exact element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<App />} />
+          <Route path="sample">
+            <Route index element={<Sample />} />
+            <Route path="other" element={<SampleOther />} />
+            <Route path=":sampleId" element={<Sample />} />
+          </Route>
+
+        </Routes>
+      </AsterControllerProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from 'react-router-dom';
+
+const Page404 = () => {
+
+    return <>
+        <h1>This is Page404 page</h1>
+    </>;
+};
+
+export default Page404;
